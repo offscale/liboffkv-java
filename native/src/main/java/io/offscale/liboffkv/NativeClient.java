ebe8235bf0c6be6c4af47f05027dd26315625b39
Raed Romanov <raid_r@mail.ru>
2020-01-08 03:19:07 +0300

    Fix typo

--- native/src/main/java/io/offscale/liboffkv/NativeClient.java
+++ native/src/main/java/io/offscale/liboffkv/NativeClient.java
@@ -2,7 +2,7 @@ package io.offscale.liboffkv;
 
 class NativeClient {
     static {
-        System.loadLibrary("offkv_java_buindings");
+        System.loadLibrary("offkv_java_bindings");
     }
 
     public native void connect(String url, String prefix);
