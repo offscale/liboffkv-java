8b2435d37c932ce620a06da8cd84bedb19c745b8
Raed Romanov <raid_r@mail.ru>
2020-01-10 06:06:37 +0300

    Minor

--- travis/bootstrap_consul.sh
+++ travis/bootstrap_consul.sh
@@ -4,12 +4,12 @@ set -e
 
 TRIPLET="$1"_amd64
 EXT=zip
-curl -L https://releases.hashicorp.com/consul/1.5.1/consul_1.5.1_${TRIPLET}.${EXT} -o ~/consul.${EXT}
+curl -L https://releases.hashicorp.com/consul/1.5.1/consul_1.5.1_${TRIPLET}.${EXT} -o $HOME/consul.${EXT}
 
-mkdir -p ~/consul
-unzip -q ~/consul.${EXT} -d ~/consul
+mkdir -p $HOME/consul
+unzip -q $HOME/consul.${EXT} -d $HOME/consul
 
-rm -f ~/consul.${EXT}
-cd ~/consul
+rm -f $HOME/consul.${EXT}
+cd $HOME/consul
 
 ./consul agent -dev > /dev/null &
